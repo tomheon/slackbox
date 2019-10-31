caeeff6081e79f3b9315dae9b3ef7eff4a421459
Edmund Jorgensen <tomheon@gmail.com>
2019-10-31 16:42:37 -0400

    Test earlier and same ts updates
    Refactor the testing code to reduce duplication meanwhile.

--- db_test.go
+++ db_test.go
@@ -7,6 +7,14 @@ import (
 	"testing"
 )
 
+func memoryDB(t *testing.T) *SlackBoxDB {
+	db, err := ConnectDB(":memory:")
+	if err != nil {
+		t.Fatalf("Error creating memory db %s", err)
+	}
+	return db
+}
+
 func bumpVersion(t *testing.T, db *SlackBoxDB) {
 	bumpVersionSql := `
     update version set version = version + 1
@@ -18,6 +26,7 @@ func bumpVersion(t *testing.T, db *SlackBoxDB) {
 }
 
 func TestErrOnUnsupportedVersion(t *testing.T) {
+	// Don't use a memory db here so we can re-run initialization
 	tempfile, err := ioutil.TempFile("", "")
 	if err != nil {
 		t.Fatalf("Could not create tempfile %s", err)
@@ -49,15 +58,27 @@ func TestErrOnUnsupportedVersion(t *testing.T) {
 	}
 }
 
-func TestUpdateConversationNew(t *testing.T) {
-	tempfile, err := ioutil.TempFile("", "")
+func checkUpdate(t *testing.T, db *SlackBoxDB, c Conversation) {
+	err := db.UpdateConversation(c)
 	if err != nil {
-		t.Fatalf("Could not create tempfile %s", err)
+		t.Fatalf("Error trying to update conversation %s", err)
 	}
+}
 
-	defer os.Remove(tempfile.Name())
+func checkGet(t *testing.T, db *SlackBoxDB, id string) Conversation {
+	c, found, err := db.GetConversation(id)
+	if !found {
+		t.Fatalf("Couldn't find conversation post update")
+	}
+	if err != nil {
+		t.Fatalf("Error trying to find conversation %s", err)
+	}
+	return c
+}
 
-	db, err := ConnectDB(tempfile.Name())
+func TestUpdateConversationNew(t *testing.T) {
+	db := memoryDB(t)
+
 	c := Conversation{ID: "someconvo", ConversationType: "im", DisplayName: "display", LatestMsgTs: "1.0000"}
 
 	_, found, err := db.GetConversation(c.ID)
@@ -68,65 +89,29 @@ func TestUpdateConversationNew(t *testing.T) {
 		t.Fatalf("Error trying to find conversation %s", err)
 	}
 
-	err = db.UpdateConversation(c)
-	if err != nil {
-		t.Errorf("Error trying to update conversation %s", err)
-	}
+	checkUpdate(t, db, c)
+	foundC := checkGet(t, db, c.ID)
 
-	foundC, found, err := db.GetConversation(c.ID)
-	if !found {
-		t.Error("Couldn't find conversation post update")
-	}
-	if err != nil {
-		t.Errorf("Error trying to find conversation %s", err)
-	}
 	if !reflect.DeepEqual(c, foundC) {
 		t.Errorf("Expected to find conversation %s, found %s", c, foundC)
 	}
 }
 
 func TestUpdateConversationLater(t *testing.T) {
-	tempfile, err := ioutil.TempFile("", "")
-	if err != nil {
-		t.Fatalf("Could not create tempfile %s", err)
-	}
+	db := memoryDB(t)
 
-	defer os.Remove(tempfile.Name())
-
-	db, err := ConnectDB(tempfile.Name())
 	c := Conversation{ID: "someconvo", ConversationType: "im", DisplayName: "display", LatestMsgTs: "1.0000"}
-	err = db.UpdateConversation(c)
-	if err != nil {
-		t.Errorf("Error trying to update conversation %s", err)
-	}
+	checkUpdate(t, db, c)
+	foundC := checkGet(t, db, c.ID)
 
-	foundC, found, err := db.GetConversation(c.ID)
-	if !found {
-		t.Error("Couldn't find conversation post update")
-	}
-	if err != nil {
-		t.Errorf("Error trying to find conversation %s", err)
-	}
 	if !reflect.DeepEqual(c, foundC) {
 		t.Errorf("Expected to find conversation %s, found %s", c, foundC)
 	}
 
 	c2 := Conversation{ID: "someconvo", ConversationType: "channel", DisplayName: "display2", LatestMsgTs: "2.0000"}
 
-	err = db.UpdateConversation(c2)
-	if err != nil {
-		t.Errorf("Error trying to update conversation %s", err)
-	}
-
-	foundC, found, err = db.GetConversation(c.ID)
-
-	if !found {
-		t.Error("Couldn't find conversation post update")
-	}
-
-	if err != nil {
-		t.Errorf("Error trying to find conversation %s", err)
-	}
+	checkUpdate(t, db, c2)
+	foundC = checkGet(t, db, c.ID)
 
 	if c2.LatestMsgTs != foundC.LatestMsgTs {
 		t.Errorf("Didn't update timestamp %s %s", c2, foundC)
@@ -141,6 +126,38 @@ func TestUpdateConversationLater(t *testing.T) {
 	}
 }
 
-// TODO test update existing with same ts
-// TODO test update existing with earlier ts
+func testNoUpdate(t *testing.T, firstTs string, secondTs string) {
+	db := memoryDB(t)
+
+	c := Conversation{ID: "someconvo", ConversationType: "im", DisplayName: "display", LatestMsgTs: firstTs}
+
+	checkUpdate(t, db, c)
+	foundC := checkGet(t, db, c.ID)
+
+	if !reflect.DeepEqual(c, foundC) {
+		t.Errorf("Expected to find conversation %s, found %s", c, foundC)
+	}
+
+	c2 := Conversation{ID: "someconvo", ConversationType: "channel", DisplayName: "display2", LatestMsgTs: secondTs}
+
+	checkUpdate(t, db, c2)
+	foundC = checkGet(t, db, c.ID)
+
+	if !reflect.DeepEqual(c, foundC) {
+		t.Errorf("Expected to find conversation %s, found %s", c, foundC)
+	}
+	if reflect.DeepEqual(c2, foundC) {
+		t.Errorf("Updated conversation mistakenly ts %s %s", c2, foundC)
+	}
+}
+
+func TestUpdateConversationEarlier(t *testing.T) {
+	testNoUpdate(t, "1.000", "0.0000")
+}
+
+func TestUpdateConversationSame(t *testing.T) {
+	// we shouldn't update on the same timestamp
+	testNoUpdate(t, "1.000", "1.000")
+}
+
 // TODO test updateconversations
